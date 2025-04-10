4c753a0a34e7321de96566255d635f1975426f20
28329753061518258 <825607153@qq.com>
2025-04-10 15:46:38 +0800

    ---

--- my_test/estate/__manifest__.py
+++ my_test/estate/__manifest__.py
@@ -12,9 +12,9 @@
     """,
     'data': [
         "security/ir.model.access.csv",
+        "views/estate_property_views_tag.xml",
         "views/estate_property_views.xml",
         "views/estate_property_views_type.xml",
-        "views/estate_property_views_tag.xml",
         "views/estate_property_views_offer.xml",
         "views/estate_menus.xml",
     ],
